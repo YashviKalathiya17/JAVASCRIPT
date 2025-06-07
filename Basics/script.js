cfd04626313fa8af42deb0f040bbc882b74948aa
YashviKalathiya17 <yashvikalathiya402@gmail.com>
2025-06-07 19:18:28 +0530

    Basic-JAVASCRIPT

---
+++ Basics/script.js
@@ -0,0 +1,39 @@
+// Print function
+console.log("Are you ready to learn JavaScript?");
+console.log("Hello world!!!");
+
+//Variable name print
+name="yashvi";
+console.log(name);
+
+name1="bhakti sharma";
+age=20;
+add="mumbai";
+console.log(name1);
+console.log(age);
+console.log(add);
+
+// null and undefined 
+
+a=null;
+b=undefined;
+console.log(a);
+console.log(b);
+
+// variable boolean
+
+isActive=true;
+console.log(isActive);
+
+//variable name rules:
+// 1. Variable names can only contain letters, numbers, underscores, and dollar signs.
+// 2. Variable names cannot start with a number.
+// 3. Variable names are case-sensitive (e.g., "myVar" and "myvar" are different).
+// 4. Variable names should not be reserved keywords (e.g., "var", "let", "const", "function", etc.).
+//Example:
+
+fullName = "Yashvi Sharma";
+fullname= "javascript";
+console.log(fullName);
+console.log(fullname);
+
