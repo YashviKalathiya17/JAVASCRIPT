7b226b21c8a5a385df35070f6e69de91d9923556
YashviKalathiya17 <yashvikalathiya402@gmail.com>
2025-07-04 19:23:39 +0530

    ArrayPractice

--- Practice/ArraysPractice/Arrayavg.js
+++ Practice/ArraysPractice/Arrayavg.js
@@ -1,19 +1,19 @@
 // Calculate an average of some Numbers
 
-// let marks = [23, 34, 45, 56, 67, 78, 89];
-// let sum = 0;
+let marks = [23, 34, 45, 56, 67, 78, 89];
+let sum = 0;
 
-// for (let i = 0; i < marks.length; i++) {
-//     sum += marks[i];
-// }
-// let avg = sum / marks.length;
-// console.log(avg);
+for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+}
+let avg = sum / marks.length;
+console.log(avg);
 
 // ---------------------print odd number------------------
-let marks = [23, 34, 45, 56, 67, 78, 89];
-for (i = 0; i < marks.length; i++) {
-    if (marks[i] % 2 !== 0) {
-        console.log("even num:", marks[i]);
+let marks1 = [23, 34, 45, 56, 67, 78, 89];
+for (i = 0; i < marks1.length; i++) {
+    if (marks1[i] % 2 !== 0) {
+        console.log("even num:", marks1[i]);
 
     }
 }
